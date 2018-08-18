fa285c89c3f8e3685785cb59942d37ab708352ed
Sarvottam <vishal.dharmawat@gmail.com>
2018-08-19 00:10:50 +0530

    ATM CARD validation

--- routes/card.js
+++ routes/card.js
@@ -29,4 +29,30 @@ router.post('/createNewCard', (req, res) =>{
             })
            }
 });
+
+router.post('/getcard', (req, res) =>{
+    var card_number = req.body.card_number;
+    var pin = req.body.pin;
+   
+    if(!card_number || ! pin ){
+    return res.status(422).json({ success: false, msg: 'card number or pin not provided' });
+    }else{
+        cardsch.findOne({card_number:card_number},function(err,data){
+            console.log(data.card_number)
+            console.log(card_number)
+            if (err){
+                return res.status(422).json({ success: false, msg: err });
+            }
+                if(data.pin == pin && data.card_number == card_number){
+                    return res.status(200).json({ success: true, msg: 'logged in successfully' });
+                }else{
+                    // console.log("data.pin is ========>",data.pin);
+                    // console.log("pin is ========>",pin);
+                    return res.status(422).json({ success: false, msg: 'incorrect pin entered' });
+
+                }
+            
+        })
+        }
+});
 module.exports = router;
