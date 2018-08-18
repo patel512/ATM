57868339309e458a6b54c5a156eb18970ed4a443
Sarvottam <vishal.dharmawat@gmail.com>
2018-08-19 01:55:34 +0530

    Recharge card-api-added

--- routes/card.js
+++ routes/card.js
@@ -15,43 +15,67 @@ router.post('/createNewCard', (req, res) =>{
     var pin = Math.floor(1000 + Math.random() * 9000);    if(!userid ){
     return res.status(422).json({ success: false, msg: 'userID not Provided' });
     }else{
-         let newCard = new cardsch({
-         userid : userid,
-         card_number:uniqid(),
-         pin:pin,
-         })
-         newCard.save((err,card) =>{
-         if(err){
-             return res.status(422).json({ success: false, msg: 'error in dcreating new card!' });
+        let newCard = new cardsch({
+        userid : userid,
+        card_number:uniqid(),
+        pin:pin,
+        })
+        newCard.save((err,card) =>{
+        if(err){
+            return res.status(422).json({ success: false, msg: 'error in dcreating new card!' });
             }else{
             return res.status(200).json({ success: true, msg: 'Card Created successfully',data: card });
                 }
             })
-           }
+        }
 });
+
+router.post('/rechargeCard',(req,res) => {
+    var card_number = req.body.card_number;
+    var amount = req.body.amount;
+    if(!card_number || amount == 0){
+        console.log("Hiiiii",card_number)
+        return res.status(422).json({ success: false, msg: 'Card Number not provided or amount is not appropriate' });
+    }
+    if(amount%100 === 0){
+    cardsch.findOne({card_number:card_number}, function(err,result){
+        if(err){
+            return res.status(422).json({ success: false, msg: err });
+        }
+        if(result){
+            // var tempBalance = (result.balance) + parseInt(amount);
+            result.balance += parseInt(amount) ;
+            result.save(function(err,result){
+                if(err){
+                    return res.status(422).json({ success: false, msg: 'Error while updating balance' });
+                }else{
+                    return res.status(200).json({ success: true, msg: 'Balance Updated Successfully',result:result });
+                }
+            })
+        }else{
+            return res.status(422).json({ success: false, msg: 'Card Number not found' });
+        }
+    })
+   }else{
+        return res.status(422).json({ success: false, msg: 'amount is not in multiple of 100' });
+   }
+})
 
 router.post('/getcard', (req, res) =>{
     var card_number = req.body.card_number;
     var pin = req.body.pin;
-   
     if(!card_number || ! pin ){
     return res.status(422).json({ success: false, msg: 'card number or pin not provided' });
     }else{
         cardsch.findOne({card_number:card_number},function(err,data){
-            console.log(data.card_number)
-            console.log(card_number)
             if (err){
                 return res.status(422).json({ success: false, msg: err });
             }
-                if(data.pin == pin && data.card_number == card_number){
-                    return res.status(200).json({ success: true, msg: 'logged in successfully' });
-                }else{
-                    // console.log("data.pin is ========>",data.pin);
-                    // console.log("pin is ========>",pin);
-                    return res.status(422).json({ success: false, msg: 'incorrect pin entered' });
-
+            if(data.pin == pin && data.card_number == card_number){
+                return res.status(200).json({ success: true, msg: 'logged in successfully',data:data });
+            }else{
+                return res.status(422).json({ success: false, msg: 'incorrect pin entered' });
                 }
-            
         })
         }
 });
